242513751aabe5f8538f5d1341b835fffabf40ae
skabone <skabone@pm.me>
2026-09-20 20:40:41 -0700

    Require confirmed unfinished games for suggested fantasy paths

--- delivery.js
+++ delivery.js
@@ -1,1 +1,1 @@
-window.NFL_DELIVERY = {"mode": "hosted", "profileRequired": true, "build": "2026-09-20-app-7", "publicUrl": "https://skabone.github.io/nfl-fantasycast/", "builtAt": "2026-09-21T03:35:28+00:00"};
+window.NFL_DELIVERY = {"mode": "hosted", "profileRequired": true, "build": "2026-09-20-app-7.1", "publicUrl": "https://skabone.github.io/nfl-fantasycast/", "builtAt": "2026-09-21T03:39:19+00:00"};
